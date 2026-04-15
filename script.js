247abee5b94f31fb34f955f66485af2f3bbfa960
Grisha101 <gambo76799@gmail.com>
2026-04-15 20:16:03 +0300

    test-website

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let a = 10;
+let b = "Hello, World!";
+const c = 3.14;
+
+var d = true; 
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+
+// alert("This is an alert message!");
+
+// let name = prompt("Please enter your name:");
+
+// alert("The value of name is: " + name);
+
+
+// let decision = confirm("Do you want to proceed?"); // Returns true or false
+
+
+
+// if (decision) {
+//     alert("You chose to proceed!");
+// } else {
+//     alert("You chose not to proceed!");
+// }
+
+
+
+
+
+
 
